9147fbbb70f0e6193dab44228ef7b2bf6b3159df
syxd.9 <syxdmatheen.9@gmail.com>
2026-05-14 15:29:49 +0530

    fix: total neural alignment to .cjs to resolve Render deployment failure and COOP warning

--- server/index.cjs
+++ server/index.cjs
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const db = require('./db');
+const db = require('./db.cjs');
 const path = require('path');
 const admin = require('firebase-admin');
 require('dotenv').config();
